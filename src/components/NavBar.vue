1fba0d6fbce2124df74da9b79fa61d8853f73160
realcold0 <realcold0@gmail.com>
2024-06-12 00:08:16 +0900

    fix : navBar template구조 변경 , 동적 화면 적용

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,26 +3,32 @@
 </script>
 <template>
     <nav class="navbar">
-      <div class="logo-section">
+      <div class="logosection">
         <RouterLink :to="{name :'home'}"><img loading="lazy" src='@/assets/img/logo.png' class="logo-img"/></RouterLink>
       </div>
-      <div class="date-section">
+
+      <div class="datesection">
         <img loading="lazy" src="@/assets/img/leftArrow.png" class="nav-icon" @click="decreaseMonth" />
         <div class="date-text">
-          <div class="month" >{{ date.month }}</div>
+          <div class="month" >{{ date.month }}월</div>
           <div class="year"><span style="font-size: 25px">{{ date.year }}</span></div>
         </div>
         <img loading="lazy" src="@/assets/img/rightArrow.png" class="nav-icon" @click="increaseMonth"/>
       </div>
-      <div class="menuSection">
-        <RouterLink :to="{name :'home'}"><img src="@/assets/img/calendar.png" alt="달력" class="nav-icon-menu"></RouterLink>
-        <RouterLink :to="{name :'expenses'}"><img src="@/assets/img/list.png" alt="소비리스트" class="nav-icon-menu"></RouterLink>
-        <RouterLink :to="{name :'analysis'}"><img src="@/assets/img/analyze.png" alt="분석" class="nav-icon-menu"></RouterLink>
+
+      <div class="bottomSection">
+          <ul  class="menuSection">
+            <li><RouterLink :to="{name :'home'}"><img src="@/assets/img/calendar.png" alt="달력" class="nav-icon-menu"></RouterLink></li>
+            <li><RouterLink :to="{name :'expenses'}"><img src="@/assets/img/list.png" alt="소비리스트" class="nav-icon-menu"></RouterLink></li>
+            <li><RouterLink :to="{name :'analysis'}"><img src="@/assets/img/analyze.png" alt="분석" class="nav-icon-menu"></RouterLink></li>
+          </ul>
+
+          <ul  class="option" >
+            <li><img loading="lazy" src ="@/assets/img/dark.png" class="nav-icon"/></li>
+            <li><RouterLink :to="{name :'profile'}"><img loading="lazy" src="@/assets/img/Account_circle.png" class="nav-icon"/></RouterLink></li>
+          </ul>
       </div>
-      <div class="option">
-        <img loading="lazy" src ="@/assets/img/dark.png" class="nav-icon"/>
-        <RouterLink :to="{name :'profile'}"><img loading="lazy" src="@/assets/img/Account_circle.png" class="nav-icon"/></RouterLink>
-      </div>
+      
     </nav>
   </template>
   
@@ -48,72 +54,80 @@ export default {
 
 <style scoped>
 .navbar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   background-color: #f7c873;
-  top: 0;
   width: 100%;
   height: 172px;
-  z-index:S1000;
-}
-
-.logo-section, .date-section, .option , .menuSection {
   display: flex;
+  justify-content: space-between;
   align-items: center;
 }
-
-.date-section {
-  flex: 0.4;
-  justify-content: center;
-  gap: 20px;
-}
-.logo-img{
-    width: 90%;
-}
-
-.nav-icon, .nav-icon-menu {
-  height: 40px; 
-}
-
-.date-text {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.month {
-  font-family: Inter, sans-serif;
-  font-weight: 700;
-  font-size: 40px;
-  font-size: normal;
-  color: #434343;
-}
-
-.year {
-  font-family: Inter, sans-serif;
-  font-weight: 400;
-  font-size: 25px;
-  color: #434343;
-  margin-top: 5px;
-}
-
-.nav-icon-menu {
-    margin : 0px 5px;
-}
-
-
-
-
 @media (max-width: 991px) {
   .navbar {
     flex-direction: column;
-    align-items: stretch;
+    align-items: center;
+    gap: 0px;
+    height: 300px;
   }
+}
 
-  .date-section {
-    justify-content: center;
-    margin: 10px 0;
-  }
+.logosection {
+  width: 293px;
+  height: 107px;
+  margin-right: 18%;
+}
+
+.datesection {
+  display: flex;
+  align-items: center;
+}
+
+.datesection img {
+  margin: 0 10px;
+}
+.month {
+  color: #434343;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+}
+.year {
+  color: #434343;
+  font-family: Inter;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-align: center;
+
+}
+
+.bottomSection{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 30%;
+  padding-right: 10px;
+}
+.bottomSection ul{
+  /* 왜 패딩 들어가있는지 모르겠음 */
+  padding-left: 0; 
+}
+
+.menuSection{
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+.menuSection li {
+  margin: 0 5px;
+}
+
+.option {
+  list-style: none;
+  display: flex;
+  align-items: center;
 }
 </style>
