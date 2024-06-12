4e36cd7f5631cb6abec96586dc0a4e5fc52b96dc
lee-junr <wns2610@icloud.com>
2024-06-12 13:09:34 +0900

    feat : 일별 사용 금액 보여주기 기능 추가

--- src/views/HomeView/components/Calendar.vue
+++ src/views/HomeView/components/Calendar.vue
@@ -1,20 +1,24 @@
 <template>
 	<div>
 		<FullCalendar :options="calendarOptions" ref="calendar" />
+		<div v-if="loading">Loading...</div>
+		<div v-else-if="error">{{ error }}</div>
 	</div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import FullCalendar from '@fullcalendar/vue3';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import koLocale from '@fullcalendar/core/locales/ko';
-import { useDateStore } from '@/stores/date'
+import { useDateStore } from '@/stores/date';
+import axios from 'axios';
 
 const calendar = ref(null);
 const dateStore = useDateStore();
-
+const loading = ref(true);
+const error = ref(null);
 const calendarOptions = ref({
 	plugins: [dayGridPlugin, interactionPlugin],
 	locale: koLocale,
@@ -25,10 +29,36 @@ const calendarOptions = ref({
 	},
 	initialView: 'dayGridMonth',
 	dateClick: handleDateClick,
-	events: [
-		{ title: 'event 1', date: '2023-04-01' },
-		{ title: 'event 2', date: '2023-04-02' }
-	]
+	events: []
+});
+
+const fetchTransactionHistory = async () => {
+	try {
+		const id = sessionStorage.getItem('id');
+		const response = await axios.get(`http://localhost:3001/transactionDetail?user_id=${id}`);
+		const transactions = response.data;
+		
+
+		// 거래 내역 데이터를 FullCalendar 이벤트 형식으로 변환
+		const events = transactions.map(transaction => ({
+			title: `${transaction.memo}`,
+			start: transaction.date,
+			extendedProps: {
+				category: transaction.category,
+				payment: transaction.payment
+			}
+		}));
+
+		calendarOptions.value.events = events;
+	} catch (err) {
+		error.value = err.message;
+	} finally {
+		loading.value = false;
+	}
+};
+
+onMounted(async () => {
+	await fetchTransactionHistory();
 });
 
 function handleDateClick(arg) {
@@ -42,5 +72,6 @@ watch(
 		calendar.value.getApi().gotoDate(`${year}-${month.toString().padStart(2, '0')}-01`);
 	}
 );
-
 </script>
+
+<style scoped></style>
